a0c29456f10886e1042916639244d2d94182b949
Evonne-Chang <lovelikeangela@hotmail.com>
2022-05-07 11:24:19 +0800

    RWD update js upadte

--- js/click_button_map.js
+++ js/click_button_map.js
@@ -1,8 +1,10 @@
 "use strict";
 
+
 let routes_button = document.getElementsByClassName('routes_button');
 for (let i = 0 ; i < routes_button.length ; i++){
     routes_button[i].addEventListener('click', mapft);
+    // routes_button[i].addEventListener('click', buttoncolor);
     routes_button[i].addEventListener('click', function(e){
         e.preventDefault();
         //console.log(routes_button);
@@ -18,7 +20,15 @@ function mapft(){
     //console.log(document.getElementById( id+'_i'));
     document.getElementsByClassName("maps_show")[0].classList.remove("maps_show");
     maps_bn.classList.add("maps_show");
-   
+
+    document.getElementsByClassName("color_ch")[0].classList.remove("color_ch");
+    // let map_a = maps_bn.children;
+    // console.log(map_a);
+    // map_a.classList.add("color_ch");
+
+    let maps_span = this.querySelector("span");
+    //console.log(maps_span);
+    maps_span.classList.add("color_ch");
     
     // console.log(maps_bn);
     // if(maps_bn.style.dispaly = "blcok"){
@@ -27,6 +37,17 @@ function mapft(){
     //     maps_bn.style.dispaly = "blcok";
     // }
 }
+
+
+
+
+// const mapa = document.querySelector(".mapa");
+// function buttoncolor(){
+//     for(let j = 0; j < mapa.length; j++){
+    
+//         mapa[i].classList.toggle("color_ch");
+//     }
+// }
 
     //let link_id = routes_button[i].getAttribute("data-id");
     // console.log(link_id);
